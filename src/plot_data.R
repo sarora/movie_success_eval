7b6abef7eac7df73045c067e09edacd6e2516e77
sarora <sidd_99@hotmail.com>
2017-12-10 14:31:04 -0800

    Add 2 new plots for budget and revenue

--- src/plot_data.R
+++ src/plot_data.R
@@ -21,7 +21,6 @@ library(cowplot)
 args <- commandArgs(trailingOnly = TRUE)
 
 input_file <- args[1]
-output_file <- args[2]
 
 main <- function() {
   
@@ -34,10 +33,11 @@ main <- function() {
   movie_data_0812 <- movie_data %>% filter(year(release_date) %in% (2008:2012))
   
   #select relevant columns
-  movie_data_last5years <- movie_data_last5years %>% select(original_title,release_date,popularity,movie_genres_primary,movie_connected_genres)
+  movie_data_last5years <- movie_data_last5years %>% select(original_title,release_date,popularity,revenue,budget,movie_genres_primary,movie_connected_genres)
   
   #select relevant columns
-  movie_data_0812 <- movie_data_0812 %>% select(original_title,release_date,popularity,movie_genres_primary,movie_connected_genres)
+  movie_data_0812 <- movie_data_0812 %>% select(original_title,release_date,popularity, revenue,budget,movie_genres_primary,movie_connected_genres)
+  
   
 
   # fig01 -- No of movies per genre between 2008-2012 and 2013-2017
@@ -56,10 +56,64 @@ main <- function() {
     coord_flip() + theme_minimal()
   
   
-  compare_plot <- plot_grid(m0812,mlast5, ncol=2)
+  compare_plot_fre <- plot_grid(m0812,mlast5, ncol=2)
   
-  ggsave(filename = output_file, plot = compare_plot, width = 10, height = 3.5, dpi = 100)
+  ggsave(filename = "../results/resources/Frequency.png", plot = compare_plot_fre, width = 10, height = 3.5, dpi = 100)
 
+  
+  ############# plot 2- Avg revenue #######################
+  
+  
+  movie_data_0812_Avg_Revenue <- movie_data_0812 %>% group_by(movie_genres_primary) %>% summarise(Total_Revenue = mean(revenue)) 
+  movie_data_last5_Avg_Revenue <- movie_data_last5years %>% group_by(movie_genres_primary) %>% summarise(Total_Revenue = mean(revenue))
+  
+  
+  
+  m_08_12_ar <- ggplot(data=movie_data_0812_Avg_Revenue, aes(x=fct_reorder((movie_genres_primary),Total_Revenue), y=Total_Revenue))+
+    geom_bar(stat="identity", fill="indianred4") +
+    scale_y_continuous(labels = scales::dollar) +
+    labs(x="Movie Genre", y = "Average Revenue per Genre", title = "Genre of Movies released between 2008-2012" ) +
+    coord_flip() + theme_minimal()
+  
+  m_last_5_ar <- ggplot(data=movie_data_last5_Avg_Revenue, aes(x=fct_reorder((movie_genres_primary),Total_Revenue), y=Total_Revenue))+
+    geom_bar(stat="identity", fill="tan2") +
+    scale_y_continuous(labels = scales::dollar) +
+    labs(x="Movie Genre", y = "Average Revenue per Genre", title = "Genre of Movies released between 2013-2017" ) + 
+    coord_flip() + theme_minimal()
+  
+  compare_plot_ar <- plot_grid(m_08_12_ar,m_last_5_ar, ncol=2)
+  
+  ggsave(filename = "../results/resources/AverageRevenue.png", plot=compare_plot_ar,width=12, height=4.5,dpi = 100)
+  
+  
+  
+  
+  ############# plot 3- Avg budget #######################
+  
+  
+  
+  movie_data_0812_Avg_Budget <- movie_data_0812 %>% group_by(movie_genres_primary) %>% summarise(Total_Budget = mean(budget)) 
+  movie_data_last5_Avg_Budget <- movie_data_last5years %>% group_by(movie_genres_primary) %>% summarise(Total_Budget = mean(budget))
+  
+  
+  
+  m_08_12_ab <- ggplot(data=movie_data_0812_Avg_Budget, aes(x=fct_reorder((movie_genres_primary),Total_Budget), y=Total_Budget))+
+    geom_bar(stat="identity", fill="indianred4") +
+    scale_y_continuous(labels = scales::dollar) +
+    labs(x="Movie Genre", y = "Average Budget per Genre", title = "Genre of Movies released between 2008-2012" ) +
+    coord_flip() + theme_minimal()
+  
+  m_last_5_ab <- ggplot(data=movie_data_last5_Avg_Budget, aes(x=fct_reorder((movie_genres_primary),Total_Budget), y=Total_Budget))+
+    geom_bar(stat="identity", fill="tan2") +
+    scale_y_continuous(labels = scales::dollar) +
+    labs(x="Movie Genre", y = "Average Budget per Genre", title = "Genre of Movies released between 2013-2017" ) + 
+    coord_flip() + theme_minimal()
+  
+  
+  ccompare_plot_b <- plot_grid(m_08_12_ab,m_last_5_ab, ncol=2)
+  
+  ggsave(filename = "../results/resources/AverageBudget.png", plot=ccompare_plot_b,width=12,height=4.5,dpi = 100)
+ 
 
 }
 
